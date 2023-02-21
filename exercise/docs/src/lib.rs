10733257bf048fc2529e45b0d4343d73c075836a
yyubin <hazing120@gmail.com>
2023-02-21 11:52:23 +0900

    Section 2.8

--- exercise/docs/src/lib.rs
+++ exercise/docs/src/lib.rs
@@ -4,9 +4,15 @@
 //
 // Once you've got the documentation here, run `cargo doc --no-deps --open` and take a look!
 
+//! A pumpkin is a vernacular term for mature winter squash of species and varieties in the genus Cucurbita that has culinary and cultural significance
+//! but no agreed upon botanical or scientific meaning.
+//! The term pumpkin is sometimes used interchangeably with "squash" or "winter squash", and is commonly used for cultivars of Cucurbita argyrosperma, Cucurbita ficifolia, Cucurbita maxima, Cucurbita moschata, and Cucurbita pepo.
+//! ![Pumpkin Image](https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/FrenchMarketPumpkinsB.jpg/700px-FrenchMarketPumpkinsB.jpg)
+
 // 2. What about an image!? Add an image of a pumpkin to the end of the module-level documentation.
 // The markdown format is ![some alt text](https://url-to-the-image.png)
 // Here's the image to link to: https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/FrenchMarketPumpkinsB.jpg/700px-FrenchMarketPumpkinsB.jpg
+
 
 // 3. Document the Pumpkin struct.
 // - The description on the index page should be "Big orange thing"
@@ -15,8 +21,14 @@
 // - Document the "roundness" field, explaining that it is a percentage
 // - Document the "orangeness" field, explaining that it is a number from 8 to 27
 
+/// Big orange thing
+/// # Recipes
+/// Recipes will be coming soon.
+
 pub struct Pumpkin {
+    /// `roundness` is a percentage
     pub roundness: f32,
+    /// `orangeness` is a number from 8 to 27
     pub orangeness: i32,
 }
 
@@ -24,16 +36,19 @@ pub struct Pumpkin {
 // can't be used for pie. :'-(
 
 impl Pumpkin {
+    ///  If you smash the pumpkin, it will be gone. Then it can't be used for pie. 😭
     pub fn smash(self) {}
 }
 
-// 5. Document that BURNT_ORANGE is for the "orangeness" field in the Pumpkin struct.
+// 5. Document that BURNT_ORANGE is for the "orangeness" field in the [`Pumpkin`] struct.
 // - Link to the Pumpkin struct in your description
 
+/// `BURNT_ORANGE` is for `orangeness` field in the Pumpkin struct.
 pub const BURNT_ORANGE: i32 = 13;
 
 // Challenge: Find the option to pass to `cargo doc` so that documentation for this private item
 // gets generated as well.  Hint: `cargo doc -h` will show you all the relevant options.
+// Answer: cargo doc --document-private-items --no-deps --open
 
 /// For internal use only. In fact, this documentation is so private that it won't be generated.
 /// At least not by default. But if you pass the correct option in, it will magically appear!
@@ -44,3 +59,5 @@ enum PrivateEnum {
     /// For dessert during North American winter holidays.
     PumpkinPie,
 }
+
+
